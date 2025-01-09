941aab214b856db9b488c0e9a62a32747e0ef0c9
Roshan Chaudhary <roshanchy313@gmail.com>
2025-01-09 19:40:34 +0545

    push

--- server/index.js
+++ server/index.js
@@ -1,37 +1,32 @@
+
 const express = require('express');
 const mongoose = require('mongoose');
-const AWS = require('aws-sdk');
+const dotenv = require('dotenv').config();
 const cors = require('cors');
 
 const app = express();
+//use express.json() to get data into json format
 app.use(express.json());
-app.use(cors());
+//Port 
 const PORT = process.env.PORT || 5500;
 
-// AWS SDK to fetch MongoDB URL
-const ssm = new AWS.SSM({ region: 'us-east-1' }); // Replace with your region
+//use cors
+app.use(cors())
+
+//import routes
+const TodoItemRoute = require('./routes/todoItems');
+
+
+//connect to mongodb ..
+mongoose.connect("mongodb+srv://roshanchy678910:8uf3Li4fC9QLijGf@cluster0.ojex6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+.then(()=> console.log("Database connected"))
+.catch(err => console.log(err))
+
+
+app.use('/', TodoItemRoute)
 
 
 
-async function getMongoDBURL() {
-    const params = { Name: '/my-app/mongodb-url', WithDecryption: true }; // Replace with your parameter name
-    const result = await ssm.getParameter(params).promise();
-    return result.Parameter.Value;
-}
+//connect to server
+app.listen(PORT, ()=> console.log("Server connected") );
 
-(async () => {
-    try {
-        const MONGO_URI = await getMongoDBURL();
-        await mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-        console.log("Database connected");
-
-
-
-        const TodoItemRoute = require('./routes/todoItems');
-        app.use('/', TodoItemRoute);
-
-        app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-    } catch (error) {
-        console.error("Error:", error);
-    }
-})();
